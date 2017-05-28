4eb1dd504085851e1d966e240ec258ade90dc13e
qRoC <qRoC.Work@gmail.com>
2017-05-28 23:19:12 +0300

    byte commits

--- Sources/Loobee/Byte/Byte.swift
+++ Sources/Loobee/Byte/Byte.swift
@@ -5,4 +5,5 @@
 // For the full copyright and license information, please view the LICENSE
 // file that was distributed with this source code.
 
+/// Unit that consists of eight bits. It is the smallest addressable unit of memory.
 public typealias Byte = UInt8
