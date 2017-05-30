5ee6169aaad23b121be9741c729bead0cac72c50
qRoC <qRoC.Work@gmail.com>
2017-05-30 11:26:38 +0300

    container protocol doc

--- Sources/Loobee/Container/Container.swift
+++ Sources/Loobee/Container/Container.swift
@@ -5,8 +5,11 @@
 // For the full copyright and license information, please view the LICENSE
 // file that was distributed with this source code.
 
+/// Protocol of a container that exposes methods to read its entries.
 public protocol Container {
+    /// Returns true if the container contains an entry that implements the specified interface.
     func has<Service>(implementationFor interface: Service.Type) -> Bool
 
+    /// Returns entry that implements the specified interface.
     func get<Service>(implementationFor interface: Service.Type) -> Service?
 }
