2e1bbfe49a5eb13521f0b4ce4c89368d5a8d1eb7
qRoC <qRoC.Work@gmail.com>
2017-07-26 01:20:52 +0300

    add -qq to apt-get update

--- Scripts/travis-clang.sh
+++ Scripts/travis-clang.sh
@@ -3,7 +3,7 @@
 set -e
 
 if [[ "$TRAVIS_OS_NAME" == "linux" ]] && [[ -n "$CLANG_VERSION" ]]; then
-    apt-get update > /dev/null
+    apt-get -qq update
     apt-get install -y clang-${CLANG_VERSION}
 
     update-alternatives --quiet --install /usr/bin/clang clang /usr/bin/clang-${CLANG_VERSION} 100
