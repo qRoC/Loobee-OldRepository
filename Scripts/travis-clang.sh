d96a4188c62f2850dbc79fcede81a2fd8f5478dc
qRoC <qRoC.Work@gmail.com>
2017-07-26 01:14:23 +0300

    detail apt-get install in trevis

--- Scripts/travis-clang.sh
+++ Scripts/travis-clang.sh
@@ -4,7 +4,8 @@ set -e
 
 if [[ "$TRAVIS_OS_NAME" == "linux" ]] && [[ -n "$CLANG_VERSION" ]]; then
     apt-get update > /dev/null
-    apt-get install -y clang-${CLANG_VERSION} > /dev/null
+    apt-get install -y clang-${CLANG_VERSION}
+
     update-alternatives --quiet --install /usr/bin/clang clang /usr/bin/clang-${CLANG_VERSION} 100
     update-alternatives --quiet --install /usr/bin/clang++ clang++ /usr/bin/clang++-${CLANG_VERSION} 100
 fi
