39f72b7f0a60027502bd2187d169ff8ff9dd3294
qRoC <qRoC.Work@gmail.com>
2017-07-26 01:30:56 +0300

    remove old clang

--- Scripts/travis-clang.sh
+++ Scripts/travis-clang.sh
@@ -3,6 +3,8 @@
 set -e
 
 if [[ "$TRAVIS_OS_NAME" == "linux" ]] && [[ -n "$CLANG_VERSION" ]]; then
+    rm -rf /usr/local/clang-3.5.0
+
     apt-get -qq update
     apt-get install -y clang-${CLANG_VERSION}
 
