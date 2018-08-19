c13dd3318f1048c281a542f6a5e466f8a56f6e61
qRoC <qRoC.Work@gmail.com>
2018-08-19 12:49:18 +0300

    install clang fix in linux travis

--- scripts/travis-clang.sh
+++ scripts/travis-clang.sh
@@ -2,13 +2,9 @@
 
 set -e
 
-if [[ "$TRAVIS_OS_NAME" == "linux" ]] && [[ -n "$CLANG_VERSION" ]]; then
+if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
     rm -rf /usr/local/clang-3.5.0
 
     apt-get -qq update
-    apt-get install -y clang-${CLANG_VERSION}
-
-    update-alternatives --quiet --install /usr/bin/clang clang /usr/bin/clang-${CLANG_VERSION} 100
-    update-alternatives --quiet --install /usr/bin/clang++ clang++ /usr/bin/clang++-${CLANG_VERSION} 100
+    apt-get install -y clang
 fi
-
